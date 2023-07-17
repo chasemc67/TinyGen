646d49a7c5c873f845d511b93dfad06bb76e67f4
chasemc67 <chasemc67@gmai.com>
2023-07-17 13:54:31 -0700

    Prompt to get relevant files appears to be working now

--- integrations/github.py
+++ integrations/github.py
@@ -1,27 +1,13 @@
-import base64
 import requests
+from dataclasses import dataclass
+from typing import List
+import re
+import pickle
 
-def get_github_files_and_contents(github_url):
-    api_url = github_url.replace('github.com', 'api.github.com/repos', 1) + '/contents/'
-    headers = {'Accept': 'application/vnd.github.v3.raw'}
-
-    try:
-        response = requests.get(api_url, headers=headers)
-        response.raise_for_status()  # Raise an exception if the response was not 200 OK
-    except requests.exceptions.HTTPError as err:
-        print(f'HTTP error occurred: {err}')  # Print the HTTP error if one occurred
-    except requests.exceptions.RequestException as err:
-        print(f'Request error occurred: {err}')  # Print the request error if one occurred
-    else:
-        file_data = response.json()  # If the request was successful, parse the response as JSON
-        print(file_data)
-        for file in file_data:
-            if file['type'] == 'file':
-                print(f"\nFile Name: {file['name']}")
-                content = requests.get(file['download_url'], headers=headers).content
-                print(f"Content: {content.decode('utf-8')}")
-            else:
-                print(f"{file['name']} is a directory")
+@dataclass
+class GithubFile:
+    filename: str
+    content: str
 
 def validate_url(url: str):
     # should match something like https://github.com/chasemc67/TinyGen
@@ -32,6 +18,38 @@ def validate_url(url: str):
     else:
         return False
 
+def get_github_files_and_contents(github_url: str) -> List[GithubFile]:
+    if not validate_url(github_url):
+        raise Exception("Invalid Github URL")
 
-# Example usage:
-# get_github_files_and_contents('https://github.com/chasemc67/TinyGen')
+    api_url = github_url.replace('github.com', 'api.github.com/repos', 1) + '/contents/'
+    headers = {'Accept': 'application/vnd.github.v3.raw'}
+
+    # Create a list to hold the GithubFile objects
+    files = []
+
+    try:
+        response = requests.get(api_url, headers=headers)
+        response.raise_for_status()  # Raise an exception if the response was not 200 OK
+    except requests.exceptions.HTTPError as err:
+        print(f'HTTP error occurred: {err}')  # Print the HTTP error if one occurred
+    except requests.exceptions.RequestException as err:
+        print(f'Request error occurred: {err}')  # Print the request error if one occurred
+    else:
+        file_data = response.json()  # If the request was successful, parse the response as JSON
+        for file in file_data:
+            if file['type'] == 'file':
+                content = requests.get(file['download_url'], headers=headers).content.decode('utf-8')
+                files.append(GithubFile(filename=file['name'], content=content))
+
+    return files  # Return the list of GithubFile objects
+
+# Read and write data which can be used for local testing instead of always hitting github API
+def write_GithubFile_list_to_file(github_files: List[GithubFile]) -> None:
+    with open('github_mock_data', 'wb') as f:
+        pickle.dump(github_files, f)
+
+def read_GithubFile_list_from_file() -> List[GithubFile]:
+    with open('github_mock_data', 'rb') as f:
+        github_files = pickle.load(f)
+    return github_files
--- langchains/openai.py
+++ langchains/openai.py
@@ -11,18 +11,32 @@ key: str = openai_api_key
 
 prompt = PromptTemplate(
     input_variables=["userPrompt", "content"],
-    template="You will be given a userPrompt and a string of conent. You must return a number between 0 and 100 for how likely you think the content is related to the user prompt. The only output you should respond with is the single number between 0 and 100. The higher the nubmer, the more likely the content is relevant to satisfy the prompt. userPrompt: {userPrompt} content: {content}"
+    template="""
+    Role: Software Engineer Assistant, makes no code changes. Only finds relevant code to be changed later.
+    You will be given a userPrompt and a string of code. 
+    The userPrompt is the larger change we're trying to make, and the code is a single file.
+    Your job is to find which files will likely be part of the larger change.
+    
+    You must return a number between 0 and 100 for how likely you think this code will need to be modified as part of the larger change. 
+    The only output you should respond with is the single number between 0 and 100. The higher the nubmer, the more likely the content is relevant to satisfy the prompt. 
+    
+    userPrompt: 
+    ```
+    {userPrompt} 
+    ```
+    
+    code: 
+    ```
+    {content}
+    ```
+
+    Answer:
+    """
 )
 
-def create_openai_llm():
-    return OpenAI(openai_api_key=key, temperature=0.1)
 
-def create_llm_agent():
-    llm = create_openai_llm()
-    tools = load_tools(["llm-math"], llm=llm)
-    agent = initialize_agent(llm=llm, tools=tools, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-    response = agent.run("what is the square root of 123531")
-    print("Agent Respone: ", response)
+def create_openai_llm():
+    return OpenAI(openai_api_key=key, temperature=0)
 
 
 def run_llm_chain_for_input(userPrompt: str, content: str) -> str:
