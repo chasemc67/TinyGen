7ebe861e8d6eae860de2f3f8175e554e2d334106
chasemc67 <chasemc67@gmai.com>
2023-07-16 17:17:40 -0700

    Cleanup: remove unused User account stuff

--- app/models.py
+++ app/models.py
@@ -1,14 +1,5 @@
 from pydantic import BaseModel
 
-class User(BaseModel):
-    name: str
-    email: str
-    password: str
-
 class RequestRecord(BaseModel):
     url: str
     prompt: str
-
-class Item(BaseModel):
-    string1: str
-    string2: str
--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 import bcrypt
 from fastapi import FastAPI
-from app.models import User, Item, RequestRecord
+from app.models import RequestRecord
 from db.supabase import create_supabase_client
 
 app = FastAPI()
 
 # init supabase client
 supabase = create_supabase_client()
-
-def user_exists(key: str = "email", value: str = None):
-    user = supabase.from_("users").select("*").eq(key, value).execute()
-    return len(user.data) > 0
-
-# Create a new user 
-@app.post("/user")
-def create_user(user: User):
-    try:
-        # Convert the email to lowercase
-        user_email = user.email.lower()
-        # hash password 
-        hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt()).decode('utf-8')
-
-        # check if user already exists
-        if user_exists(value=user_email):
-            return {"message": "User already exists"}
-        
-        # Add user into users table
-        user = supabase.from_("users")\
-            .insert({"name": user.name, "email": user_email, "password": hashed_password})\
-            .execute()
-
-        if user:
-            return {"message": "User created successfully"}
-        else: 
-            return {"message": "User creation failed"}
-    except Exception as e:
-        print("Error: ", e)
-        return {"message": "User creation failed"}
 
 # Append to the history table log
 @app.post("/generate")
@@ -54,8 +24,3 @@ def record_request(request: RequestRecord):
     except Exception as e:
         print("Error: ", e)
         return {"message": "Request recording failed"}
-
-@app.post("/concatenate/")
-async def concatenate_strings(item: Item):
-    result = item.string1 + item.string2
-    return {"result": result}
